ea1d4576ddeee1e9ac049a296bd37ad718dd3d59
JKC-013 <126439561+JKC-013@users.noreply.github.com>
2026-08-28 10:05:34 +0700

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { createClient } from '@libsql/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
 const libsql = createClient({
   url: process.env.TURSO_DATABASE_URL!,
